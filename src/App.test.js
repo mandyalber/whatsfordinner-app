9c3ed47bd5fa0335451e30b58cb951c459b04ddc
Mandy Alber <mandy.alber@gmail.com>
2020-12-20 10:44:49 -0800

    updated api endpoint

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
-});
+//smoke test
+test('renders App without crashing', () => {
+  render(<BrowserRouter><App /></BrowserRouter>);
+}); 
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-    //API_ENDPOINT: `https://infinite-bayou-90165.herokuapp.com/api`,
-    API_ENDPOINT: `http://localhost:8000/api`,
+    API_ENDPOINT: `https://infinite-bayou-90165.herokuapp.com/api`,
+    //API_ENDPOINT: `http://localhost:8000/api`,
     TOKEN_KEY: 'wfd-client-auth-token'
     // https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies
   }
