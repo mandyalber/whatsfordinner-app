cd67e49cfd2766db715368a7fb36f2ffd7c9740e
Mandy Alber <mandy.alber@gmail.com>
2020-12-21 13:35:22 -0800

    made some user feedback styling updates

--- src/components/LandingPage/LandingPageNav.test.js
+++ src/components/LandingPage/LandingPageNav.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import LandingPage from './LandingPage';
 
 //smoke test
 test('renders LandingPage without crashing', () => {
-  render(<LandingPage />);
+  render(<BrowserRouter><LandingPage /></BrowserRouter>);
 });
